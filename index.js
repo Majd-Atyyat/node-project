4a42846845a8877f3d0682730a9172a6d6675d32
majd atyyat <majd.atyyat@pwc.com>
2023-04-04 07:03:47 +0300

    added authentication

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pg = require('pg');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,13 +14,35 @@ const config = {
     database: "majd",
 };
 
+const JWT_SECRET = process.env.JWT_SECRET || "WIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1hamFyZCIsImlhdC";
 const pool = new pg.Pool(config);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Create a middleware function to verify the JWT in the request header
+
+
+
+function authenticate(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Authorization header missing" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
+
+
 // GET all items
-app.get('/models', (req, res) => {
+app.get('/models', authenticate, (req, res) => {
   pool.query('SELECT * FROM model', (err, result) => {
     if (err) {
       throw err;
@@ -30,7 +53,7 @@ app.get('/models', (req, res) => {
 });
 
 // GET one item by id
-app.get('/models/:id', (req, res) => {
+app.get('/models/:id', authenticate, (req, res) => {
   const id = req.params.id;
 
   pool.query(`SELECT * FROM model WHERE id = ${id}`, (err, result) => {
@@ -43,7 +66,7 @@ app.get('/models/:id', (req, res) => {
 });
 
 // POST a new item
-app.post('/models', (req, res) => {
+app.post('/models', authenticate, (req, res) => {
   const { name, description } = req.body;
 
   pool.query(
@@ -59,7 +82,7 @@ app.post('/models', (req, res) => {
 });
 
 // PUT (update) an existing item
-app.put('/models/:id', (req, res) => {
+app.put('/models/:id', authenticate, (req, res) => {
   const id = req.params.id;
   const { name, description } = req.body;
 
@@ -76,7 +99,7 @@ app.put('/models/:id', (req, res) => {
 });
 
 // DELETE an existing item
-app.delete('/items/:id', (req, res) => {
+app.delete('/models/:id', authenticate, (req, res) => {
   const id = req.params.id;
 
   pool.query(`DELETE FROM model WHERE id = ${id}`, (err, result) => {
@@ -87,6 +110,23 @@ app.delete('/items/:id', (req, res) => {
     res.send(`model with ID: ${id} deleted`);
   });
 });
+
+// Authorization 
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    // Check if username and password are valid
+    if (username !== "admin" || password !== "password") {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+  
+    // Generate a JWT with a payload containing the username and expiration date
+    const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: "100h" });
+  
+    // Return the JWT in the response
+    res.json({ token });
+  });
+  
 
 // Start the server
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
